c78cbe4d682130e5656890cbc3953780aae54464
Sella Rafaeli <sella.rafaeli@gmail.com>
2022-05-27 21:58:44 +0300

    auto-message

--- bl/blog.rb
+++ bl/blog.rb
@@ -3,6 +3,11 @@ def render_blog_post(post, opts = {})
 	erb :"/blog/blog_post_template", locals: {post: post}.merge(opts)
 end
 
+def post_link(post)
+  '/blog/'+post[:title].gsub(' ','-')
+end
+
+
 get '/blog/?:id?' do 
 	erb :'blog/blog', default_layout
 end
