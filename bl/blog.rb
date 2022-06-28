0cdea98fd082ce7ab54439a7c7934827689c7e6b
Sella Rafaeli <sella.rafaeli@gmail.com>
2022-06-28 23:05:41 +0300

    Blog v2

--- bl/blog.rb
+++ bl/blog.rb
@@ -1,14 +1,41 @@
-def render_blog_post(post, opts = {})	
-	# post[:num_views] ||= num_pageviews(post[:path])
-	erb :"/blog/blog_post_template", locals: {post: post}.merge(opts)
-end
+$blog = $mongo.collection('blog')
 
-def post_link(post)
-  '/blog/'+post[:title].gsub(' ','-')
-end
-
-
-get '/blog/?:id?' do 
+get '/blog' do 
 	erb :'blog/blog', default_layout
 end
 
+get '/blog/:id/:title' do 
+	post = $blog.get(pr[:id])
+	erb :'blog/single_post', default_layout
+end
+
+get '/blog/test' do 
+	erb :'blog/editable_post_template'
+end
+
+# admin 
+
+post '/admin/blog/new_post' do 
+	title = pr[:title]
+	body  = erb :'blog/editable_post_template'
+	
+	$blog.add({title: title, body: body})
+	redirect back
+end
+
+post '/admin/blog/edit_post' do 
+	id    = pr[:_id]
+	title = pr[:title]	
+	body  = pr[:body]
+
+	body  = CGI.unescapeHTML(body)
+	$blog.update_id(id, title: title, body: body)
+	{msg: 'ok'}
+end
+
+post '/admin/blog/:id/toggle' do 
+	id   = pr[:id]
+	post = $blog.get(id)
+	$blog.update_id(id, hidden: !post[:hidden])
+	redirect back
+end
---
+++ bl/blog1.rb
@@ -0,0 +1,14 @@
+def render_blog_post(post, opts = {})	
+	# post[:num_views] ||= num_pageviews(post[:path])
+	erb :"/blog/blog_post_template", locals: {post: post}.merge(opts)
+end
+
+def post_link(post)
+  '/blog/'+post[:title].gsub(' ','-')
+end
+
+# old!
+# get '/blog/?:id?' do 
+# 	erb :'blog/blog', default_layout
+# end
+
