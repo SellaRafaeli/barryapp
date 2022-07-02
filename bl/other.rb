00b1257933817f120a3478abcba4c231d80e9cf9
Sella Rafaeli <sella.rafaeli@gmail.com>
2022-07-02 13:47:59 +0300

    auto-message

--- bl/other.rb
+++ bl/other.rb
@@ -26,18 +26,20 @@ get '/pricing' do
 	erb :'rafaeli/homepage/pricing', default_layout
 end
 
-get '/deck' do 
+get '/deck/:code' do 
 	code = pr[:code]
 	deck_codes = {
-		nicole_ibex: true
+		nicole_ibex: true,
+		omri_sheva: true
 	}.hwia
 
 	if !deck_codes[code] 
 		halt(401, "Please contact ir@indydevs.com for access.")
 	end
 
-	msg = "deck view: #{code}"+Time.now.to_s
+	msg = "deck view: #{code} "+Time.now.to_s
 	send_email('sella@indydevs.com', msg, msg)
 
-	redirect 'https://docs.google.com/presentation/d/18MxHxayh9YKQ5FIrE04lfYchN5jFrnsSz0mfdhw-GEk/edit?usp=sharing'
+	send_file(Dir.pwd+'/files/minideck_jul_2.pdf')
+	#redirect 'https://docs.google.com/presentation/d/18MxHxayh9YKQ5FIrE04lfYchN5jFrnsSz0mfdhw-GEk/edit?usp=sharing'
 end
